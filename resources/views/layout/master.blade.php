4a8bc499143be2b62dff337eea3e799f595de681
Willem Van Bockstal <willem@spatie.be>
2017-10-26 22:08:33 +0200

    Update master.blade.php
    - title tag
    - meta viewport

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -3,10 +3,11 @@
 
 <head>
     <link rel="dns-prefetch" href="https://fonts.googleapis.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover">
+    <title>DNS records lookup | dnsrecords.io</title>
     <link href="https://fonts.googleapis.com/css?family=Fira+Mono:400,700" rel="stylesheet">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <script src="{{ mix('/js/app.js') }}" defer></script>
-
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
