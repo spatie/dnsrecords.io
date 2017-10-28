3b4621689dc4bc601a52cd7f396d547a91a61f4b
freek <freek@spatie.be>
2017-10-28 11:16:56 +0200

    fix color

--- app/Services/Commands/Commands/Manual.php
+++ app/Services/Commands/Commands/Manual.php
@@ -21,7 +21,7 @@ class Manual implements Command
             "Enter 'doom' to play Doom.",
         ])->implode('<br>');
 
-        flash()->message($manualText);
+        flash()->message($manualText, 'message');
 
         return back();
     }
