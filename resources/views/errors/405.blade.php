2bbb1eec924dfee2700b40d33c2bef6e1332eaf3
Willem Van Bockstal <willem@spatie.be>
2017-10-30 23:21:10 +0100

    Typo

--- resources/views/errors/405.blade.php
+++ resources/views/errors/405.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', 'Method not allowed')
 
-@section('message', 'I\'m sorry, I was not build to do stuff like this.')
+@section('message', 'I\'m sorry, I was not built to do stuff like this.')
