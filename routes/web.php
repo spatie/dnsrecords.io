0fd41678e78c4e2f52982ed5b11fe507909389c9
Sven Luijten <svenluijten1995@gmail.com>
2017-10-30 15:04:52 +0100

    catch everything in command route

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
 <?php
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::match(['get', 'post'], '/{command}', 'HomeController@submit')->middleware('logRequest');
+Route::match(['get', 'post'], '/{command}', 'HomeController@submit')->middleware('logRequest')->where('command', '\S+');
